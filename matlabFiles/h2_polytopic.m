3c5a619c79e272c6cc40a803fcd7d2c08bf7bebc
Neuhaus <tassianors@gmail.com>
2010-09-23 00:54:09 -0300

    add politopyc simulation to H2 norm.

---
+++ matlabFiles/h2_polytopic.m
@@ -0,0 +1,132 @@
+%Trabalho - Controle Robusto 
+% LMIs - H2 - Polytopic
+% init
+clear all;close all;
+
+% limits
+a=[-0.25 -2];
+k=[-0.4649e-4 -0.7449e-4];
+b=-0.012725;
+
+% middle of uncertainties
+A0=[[0 1];[-b*(a(1)+a(2))/2 b+(a(1)+a(2))/2]]
+B0=[0;(k(1)+k(2))/2]
+
+% System description - polytopic
+A1=[[0 1];[-b*a(1) b+a(1)]]
+A2=[[0 1];[-b*a(2) b+a(2)]]
+B1=[0;k(1)]
+B2=[0;k(2)]
+Bw=B1;%for convinience
+% I choose this value
+C=[1 0];
+ 
+% z=(G-HK)x
+G=C;
+H=0;
+% get usefuul dimensions
+[n,m]=size(B1);
+%========================================
+% Init LMI's descriptions
+setlmis([]);
+
+% var descriptions
+W=lmivar(1,[n 1]);
+R=lmivar(2,[m,n]);
+% M is a scalar
+M=lmivar(1,[1 1]);
+
+%gamma=lmivar(1,[1,1]);
+%========================================
+% equation 1 - A1 B1
+lmi1a=newlmi;
+% a11
+lmiterm([-lmi1a 1 1 W],-A1,1,'s'); % Symmetric
+lmiterm([-lmi1a 1 1 R],1,B1, 's'); % Symetric 
+% a12 = (G*W-H*R)'
+lmiterm([-lmi1a 1 2 W],1,G');      % W*G'
+lmiterm([-lmi1a 1 2 -R],1,-H');    % -R'*H'
+%a22
+lmiterm([-lmi1a 2 2 0],1);         % I
+% A33
+lmiterm([-lmi1a 3 3 0],1);         % I
+%========================================
+% equation 2 - A1 B2
+lmi1b=newlmi;
+% a11
+lmiterm([-lmi1b 1 1 W],-A1,1,'s'); % Symmetric
+lmiterm([-lmi1b 1 1 R],1,B2, 's'); % Symetric 
+% a12 = (G*W-H*R)'
+lmiterm([-lmi1b 1 2 W],1,G');      % W*G'
+lmiterm([-lmi1b 1 2 -R],1,-H');    % -R'*H'
+%a22
+lmiterm([-lmi1b 2 2 0],1);         % I
+% A33
+lmiterm([-lmi1b 3 3 0],1);         % I
+%========================================
+% equation 3 - A2 B1
+lmi1c=newlmi;
+% a11
+lmiterm([-lmi1c 1 1 W],-A2,1,'s'); % Symmetric
+lmiterm([-lmi1c 1 1 R],1,B1, 's'); % Symetric 
+% a12 = (G*W-H*R)'
+lmiterm([-lmi1c 1 2 W],1,G');      % W*G'
+lmiterm([-lmi1c 1 2 -R],1,-H');    % -R'*H'
+%a22
+lmiterm([-lmi1c 2 2 0],1);         % I
+% A33
+lmiterm([-lmi1c 3 3 0],1);         % I
+%========================================
+% equation 4 - A2 B2
+lmi1d=newlmi;
+% a11
+lmiterm([-lmi1d 1 1 W],-A2,1,'s'); % Symmetric
+lmiterm([-lmi1d 1 1 R],1,B2, 's'); % Symetric 
+% a12 = (G*W-H*R)'
+lmiterm([-lmi1d 1 2 W],1,G');      % W*G'
+lmiterm([-lmi1d 1 2 -R],1,-H');    % -R'*H'
+%a22
+lmiterm([-lmi1d 2 2 0],1);         % I
+% A33
+lmiterm([-lmi1d 3 3 0],1);         % I
+%========================================
+
+lmi2=newlmi;
+lmiterm([-lmi2 1 1 W],1,1);      % W > 0
+%========================================
+lmi3=newlmi;
+lmiterm([-lmi3 1 1 M],1,1);
+lmiterm([-lmi3 1 2 0],Bw');
+lmiterm([-lmi3 2 2 W],1,1);
+%========================================
+% finish relationship description
+nomesis = getlmis;
+
+% solve
+no = decnbr(nomesis);
+co = zeros(no,1);
+for j=1:no,
+[Mj] = defcx(nomesis,j,M);
+co(j) = trace(Mj);
+end
+
+%Executa a minimiza�ao do criterio
+[copt,gopt] = mincx(nomesis,co);
+
+% Get numerical results
+W_o=dec2mat(nomesis,gopt,W)
+R_o=dec2mat(nomesis,gopt,R)
+M_o=dec2mat(nomesis,gopt,M)
+% get K feedback matrix
+K=R_o*inv(W_o)
+
+% compare solutions
+autoA=eig(A0)
+autoABK=eig(A0-B0*K)
+
+A0_B0=ss(A0-B0*K,B0,C,0)
+A1_B1=ss(A1-B1*K,B1,C,0)
+A1_B2=ss(A1-B2*K,B2,C,0)
+A2_B2=ss(A2-B2*K,B2,C,0)
+A2_B1=ss(A2-B1*K,B1,C,0)
+step(A0_B0,A1_B1, A1_B2, A2_B1,  A2_B2)
